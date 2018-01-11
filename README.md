86ccf630b5409f875ed5c8a27290254335c70d68
= <=>
2018-01-11 18:03:50 -0500

    Fixed issues with opening multiple popups at the same time from inside another popup. Also fixed some css issues

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
   
 :black_square_button: **React Popup Component** is a simple animated popup that creates a wrapper component around your text and lets you pass down whatever content you wish inside props.children.
 
-:construction: UNDER CONSTRUCTION : Fixing CSS Issues :construction:
+:construction: UNDER CONSTRUCTION : Check back soon :construction:
--- src/index.js
+++ src/index.js
@@ -6,63 +6,72 @@ export default class Popup extends Component {
         super(props);
         this.props = props;
         this.width = this.props.width || '400px';
+        this.height = this.props.height || '400px';
         this.popupBackground;
         this.myPopup;
         this.closeButton;
         this.pos = 0;
-        this.togglePopupComponent = this.togglePopupComponent.bind(this);
+        this.openPopupComponent = this.openPopupComponent.bind(this);
         this.close = this.close.bind(this);
-        this.which = this.props.which || '1';
+        this.which = this.props.which;
+        this.LCM;
     }
 
-    togglePopupComponent(which){
+    openPopupComponent(which){
         //note: the CSS only loops the .popupBackgroundClass(number) up to 10. If you need a higher number of popups with this you will have to modify the CSS
         this.which = which;
         //'which' refers to which props.children we want to work on
         this.popupBackground = document.querySelector(`.popupBackgroundClass-${this.which}`);
-        this.popupBackground.classList.toggle('hide');
-        this.myPopup.classList.toggle('zeroSize');
-        this.pos = 0;
-        this.animate();
+        //make sure it exists
+
+        console.log('this.popupBackground', this.popupBackground);
+
+        (this.popupBackground) ? this.popupBackground.classList.remove('hide') : null;
+
+        //this ensures that when a popup is opened by another popup, then all open popups that are not supposed to be active are closed
+
+        let allBackgrounds = document.querySelectorAll('.popupBackground');
+
+        for(let i=0; i<allBackgrounds.length; i++){
+            let _current = `.popupBackgroundClass-${which}`;
+            let _test = `.popupBackgroundClass-${i}`;
+
+            console.log('i', i);
+
+            console.log(`.popupBackgroundClass-${i}`);
+            console.log('test', _test);
+            (_current !== _test) ? document.querySelector(`.popupBackgroundClass-${i}`).classList.add('hide') : null;
+        }
     }
 
-    animate(){
-        //set elem to the class that was passed down with the variable 'which'
-        let elem = document.querySelector(`.popupContentClass-${this.which}`);
-        //resets the width and minHeight to the value of 'this.pos' (zero)
-        elem.style.minHeight = this.pos;
-        elem.style.width = this.pos;
-
-        let id = setInterval(() => {
-            let exactWidth = this.width;
-            //removes the 'px' from the end if the passed down width
-            if (this.pos >= exactWidth.substring(0, exactWidth.length - 2)) {
-                clearInterval(id);
-            } else {
-                this.pos = this.pos + 10;
-                elem.style.width = this.pos + 'px';
-                elem.style.minHeight = this.pos + 'px';
-            }
-        }, 5);
-
-    }
     componentDidMount(){
         //in case no specific popup was passed down with 'which', it defaults to 1 on mount
         this.popupBackground = document.querySelector(`.popupBackgroundClass-${this.which}`);
         // document.body.insertBefore(this.popupBackground, document.body.firstChild);
+
+        let instances = document.querySelectorAll(`.popupBackgroundClass-${this.which}`);
+        console.log('instances', instances);
     }
     close(e){
+
+        //e.target === this.popupBackground || e.target === this.xButton
         //to allows for things suc has inputs or clickable events inside of the component, we only close the popup if the background or the 'X' button is clicked
-        if(e.target === this.popupBackground || e.target === this.xButton) {
-            this.popupBackground.classList.toggle('hide');
-            this.myPopup.classList.toggle('zeroSize');
+
+        let instances = document.querySelectorAll(`.popupBackgroundClass-${this.which}`);
+        console.log('instances', instances);
+
+        for(let i=0; i<instances.length; i++){
+            instances[i].classList.add('hide');
         }
+
+        //turned off while the animator is turned off
+        // this.myPopup.classList.toggle('zeroSize');
     }
     render(){
         return(
-            <div onClick={(e)=>this.close(e)} className={`popupBackgroundClass-${this.which} hide`}>
+            <div onClick={(e)=>this.close(e)} className={`popupBackground popupBackgroundClass-${this.which} hide`}>
                 <div id='myPopupComponent'
-                     style={{width: 0, height: 0}}
+                     style={{width: this.props.width, height: '500px'}}
                      className={`popupContentClass-${this.which}`} //zeroSize
                      ref={ref => this.myPopup = ref}>
                     <img ref={ref => this.xButton = ref} src='images/closeButton.svg' className='closeButtonImage'/>
@@ -72,3 +81,5 @@ export default class Popup extends Component {
         )
     }
 }
+
+
